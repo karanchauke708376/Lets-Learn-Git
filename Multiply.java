339b3f5e9cb16dace9d921cc9b1f078752a9516c
Karan Chauke <69950796+karanchauke708376@users.noreply.github.com>
2022-04-16 10:57:01 +0530

    Adding Multiply.java file

---
+++ Multiply.java
@@ -0,0 +1,22 @@
+package GitHub_Files;
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Two Numbers");
+		
+		System.out.println("Enter A Values : ");
+		int a = sc.nextInt();
+		System.out.println("Enter B Values : ");
+		int b = sc.nextInt();
+		
+		int c = a * b ;
+		
+		System.out.println(c + " ");
+	}
+
+}
