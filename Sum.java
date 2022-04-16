a3bde5c78c70f7964f19e3889405de276c158b66
Karan Chauke <69950796+karanchauke708376@users.noreply.github.com>
2022-04-16 10:21:00 +0530

    Initial Commit

---
+++ Sum.java
@@ -0,0 +1,22 @@
+package GitHub_Files;
+import java.util.Scanner;
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Two Numbers");
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int c = a + b ;
+		
+		System.out.println(c + " ");
+		
+		
+		
+	}
+
+}
